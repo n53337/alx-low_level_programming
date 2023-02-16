3fb7b29f3637e2f986bd0c45708b915627e088f2
n53337 <scalewithusef@gmail.com>
2023-02-16 04:27:00 -0800

    fix #8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,8 @@
  */
 int main(void)
 {
-    char my_string[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(STDOUT_FILENO, my_string, strlen(my_string));
-    return 1;
+	char x[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, x, strlen(x));
+	return (1);
 }
