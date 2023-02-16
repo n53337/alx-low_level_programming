6d3629447fdbebd0225a438f750b6d4e355b5137
n53337 <scalewithusef@gmail.com>
2023-02-16 01:09:09 -0800

    fix #5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
+/**
+ *  * Description: main - Prints out a string to stdout.
+ *   * Return: 0 if success.
+*/
 
 int main(void)
 {
-	/* this function print something */
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
