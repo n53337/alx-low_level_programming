5d6f6b9c2d1e845288c9d4c4bc2968c6c21c80ab
n53337 <scalewithusef@gmail.com>
2023-02-16 00:53:32 -0800

    fix #4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-/* this function print something */
 int main(void)
 {
+	/* this function print something */
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
